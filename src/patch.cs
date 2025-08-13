8d77a3195f4eafeda9cccc81560ead5fd6f715aa
Evelyn Veilwinter <evelyntsmg@gmail.com>
2025-08-13 15:51:32 -0300

    Fix: Correct access modifiers _again_

--- src/patch.cs
+++ src/patch.cs
@@ -4,7 +4,7 @@ using Fahrenheit.Core;
 
 namespace Fahrenheit.Modules.EFP;
 
-internal abstract class EfpPatch {
+public abstract class EfpPatch {
     public abstract FhSettingsCategory get_settings();
 
     public abstract void hook();
